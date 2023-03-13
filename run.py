68e725d630c0d68fa7ecde8112321d8048c5f2e4
georgiagrayland <georgiamayeva@live.co.uk>
2023-03-13 15:14:31 +0000

    add url links

--- run.py
+++ run.py
@@ -5,9 +5,19 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/")  # This forward slash is for the homepage
 def index():
     return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 
 if __name__ == "__main__":
